dc87e83bf9efc25920f9e8d581b7ac5f34feaf34
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-12-09 15:55:17 +0700

    Move more specific `filterIt()` declaration up

--- src/transformation/filter-it.ts
+++ src/transformation/filter-it.ts
@@ -8,23 +8,6 @@ import type { PushIterable } from '../push-iterable';
 import type { PushIterator } from '../push-iterator';
 
 /**
- * Creates a {@link PushIterable | push iterable} with all `source` iterable elements that pass the test implemented by
- * the provided function.
- *
- * @typeParam T - A type of source elements.
- * @param source - A source iterable.
- * @param test - A predicate function to test each element. Returns `true` to keep the element, or `false` otherwise.
- * It accepts the tested element as the only parameter.
- *
- * @returns New push iterable with the elements that pass the test. If no elements passed the test, an empty iterable
- * will be returned.
- */
-export function filterIt<T>(
-    source: Iterable<T>,
-    test: (this: void, element: T) => boolean,
-): PushIterable<T>;
-
-/**
  * Creates a {@link PushIterable | push iterable} with all `source` iterable elements extending the given type.
  *
  * @typeParam T - A type of source elements.
@@ -40,6 +23,23 @@ export function filterIt<T, TTarget extends T>(
     source: Iterable<T>,
     test: (this: void, element: T) => element is TTarget,
 ): PushIterable<TTarget>;
+
+/**
+ * Creates a {@link PushIterable | push iterable} with all `source` iterable elements that pass the test implemented by
+ * the provided function.
+ *
+ * @typeParam T - A type of source elements.
+ * @param source - A source iterable.
+ * @param test - A predicate function to test each element. Returns `true` to keep the element, or `false` otherwise.
+ * It accepts the tested element as the only parameter.
+ *
+ * @returns New push iterable with the elements that pass the test. If no elements passed the test, an empty iterable
+ * will be returned.
+ */
+export function filterIt<T>(
+    source: Iterable<T>,
+    test: (this: void, element: T) => boolean,
+): PushIterable<T>;
 
 export function filterIt<T>(
     source: Iterable<T>,
