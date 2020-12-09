c7ddfd9083a7f555ff9ed8a18f25c07b274508df
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-12-09 15:56:58 +0700

    Move more specific `filterArray()` declaration up

--- src/transformation/filter-array.ts
+++ src/transformation/filter-array.ts
@@ -8,23 +8,6 @@ import type { PushIterable } from '../push-iterable';
 import { iterateOverFilteredIndexed } from './iterate-over-filtered-indexed.impl';
 
 /**
- * Creates a {@link PushIterable | push iterable} with all `array` elements that pass the test implemented by
- * the provided function.
- *
- * @typeParam T - A type of array elements.
- * @param array - A source array.
- * @param test - A predicate function to test each element. Returns `true` to keep the element, or `false` otherwise.
- * It accepts the tested element as the only parameter.
- *
- * @returns New push iterable with the elements that pass the test. If no elements passed the test, an empty iterable
- * will be returned.
- */
-export function filterArray<T>(
-    array: ArrayLike<T>,
-    test: (this: void, element: T) => boolean,
-): PushIterable<T>;
-
-/**
  * Creates a {@link PushIterable push iterable} with all `array` elements extending the given type.
  *
  * @typeParam T - A type of array elements.
@@ -40,6 +23,23 @@ export function filterArray<T, TTarget extends T>(
     array: ArrayLike<T>,
     test: (this: void, element: T) => element is TTarget,
 ): PushIterable<TTarget>;
+
+/**
+ * Creates a {@link PushIterable | push iterable} with all `array` elements that pass the test implemented by
+ * the provided function.
+ *
+ * @typeParam T - A type of array elements.
+ * @param array - A source array.
+ * @param test - A predicate function to test each element. Returns `true` to keep the element, or `false` otherwise.
+ * It accepts the tested element as the only parameter.
+ *
+ * @returns New push iterable with the elements that pass the test. If no elements passed the test, an empty iterable
+ * will be returned.
+ */
+export function filterArray<T>(
+    array: ArrayLike<T>,
+    test: (this: void, element: T) => boolean,
+): PushIterable<T>;
 
 export function filterArray<T>(
     array: ArrayLike<T>,
