c176234c6e747dfea1193e001c4f0f9add141a21
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-07-14 15:49:58 +0700

    Drop `pushHead()`

--- src/base/index.ts
+++ src/base/index.ts
@@ -3,5 +3,4 @@ export * from './iterate-generated';
 export * from './iterate-it';
 export * from './iterator-of';
 export * from './make-push-iterable';
-export * from './push-head';
 export * from './push-iterated';
--- src/base/iterate-it.ts
+++ src/base/iterate-it.ts
@@ -1,8 +1,8 @@
+import { PushIterator__symbol } from '../push-iterable';
 import type { PushIterator } from '../push-iterator';
 import { isPushIterable } from './is-push-iterable';
 import { iterateArray } from './iterate-array.impl';
 import { iteratorOf } from './iterator-of';
-import { pushHead } from './push-head';
 import { PushIterator$empty } from './push-iterator.empty.impl';
 import { rawIteratorPusher, toPushIterator } from './raw-iterator.impl';
 
@@ -11,8 +11,6 @@ import { rawIteratorPusher, toPushIterator } from './raw-iterator.impl';
  *
  * Calls `accept` method for each iterated element until there are elements to iterate, or `accept` returned either
  * `true` or `false`.
- *
- * In contrast to {@link pushHead} function, this one accepts any iterable instance.
  *
  * @typeParam T - Iterated elements type.
  * @param iterable - An iterable to iterate elements of.
@@ -24,7 +22,7 @@ import { rawIteratorPusher, toPushIterator } from './raw-iterator.impl';
  */
 export function iterateIt<T>(iterable: Iterable<T>, accept: PushIterator.Acceptor<T>): PushIterator<T> {
   if (isPushIterable(iterable)) {
-    return pushHead(iterable, accept);
+    return iterable[PushIterator__symbol](accept);
   }
   if (Array.isArray(iterable)) {
     return arrayHead(iterable, accept);
@@ -44,7 +42,7 @@ function rawIterableHead<T>(
   const it = iteratorOf(iterable);
 
   if (isPushIterable(it)) {
-    return pushHead(it, accept);
+    return it[PushIterator__symbol](accept);
   }
 
   const forEach = rawIteratorPusher(it);
