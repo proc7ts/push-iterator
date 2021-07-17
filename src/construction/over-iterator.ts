a2c912aff2bf482ef535e67de2f8063534633ce1
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-07-17 09:36:29 +0700

    `overIterator()`: Respect iteration mode

--- src/construction/over-iterator.ts
+++ src/construction/over-iterator.ts
@@ -1,8 +1,9 @@
 import { isPushIterable, makePushIterable } from '../base';
 import { iterator$convert, iterator$pusher } from '../base/iterator.impl';
+import { PushIterator$empty } from '../base/push-iterator.empty.impl';
 import type { PushIterable } from '../push-iterable';
 import { PushIterator__symbol } from '../push-iterable';
-import { overNone } from './over-none';
+import { PushIterationMode } from '../push-iteration-mode';
 
 /**
  * Creates a {@link PushIterable | push iterable} over elements of iterator created by the given function.
@@ -17,16 +18,18 @@ export function overIterator<T>(iterate: (this: void) => Iterator<T>): PushItera
 }
 
 function overIterator$iterate<T>(iterate: (this: void) => Iterator<T>): PushIterable.Iterate<T> {
-  return accept => {
+  return (accept, mode = PushIterationMode.Some) => {
 
     const it = iterate();
 
     if (isPushIterable(it)) {
-      return it[PushIterator__symbol](accept);
+      return it[PushIterator__symbol](accept, mode);
     }
 
     const forNext = iterator$pusher(it);
 
-    return accept && !forNext(accept) ? overNone() : iterator$convert(it, forNext);
+    return accept && !forNext(accept)
+        ? PushIterator$empty
+        : iterator$convert(it, forNext);
   };
 }
