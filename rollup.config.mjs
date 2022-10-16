d0fdff5302dae827af3532ef61accdf5ab5dea48
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-10-16 12:21:18 +0700

    Upgrade to Rollup 3.0

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,6 @@ import { externalModules } from '@run-z/rollup-helpers';
 import path from 'node:path';
 import { defineConfig } from 'rollup';
 import flatDts from 'rollup-plugin-flat-dts';
-import sourcemaps from 'rollup-plugin-sourcemaps';
 import ts from 'rollup-plugin-typescript2';
 import typescript from 'typescript';
 
@@ -17,22 +16,21 @@ export default defineConfig({
       tsconfig: 'tsconfig.main.json',
       cacheRoot: 'target/.rts2_cache',
     }),
-    sourcemaps(),
   ],
   external: externalModules(),
-  manualChunks(id) {
-    if (id.startsWith(path.resolve('src', 'call-thru') + path.sep)) {
-      return 'push-iterator.call-thru';
-    }
-
-    return 'push-iterator';
-  },
   output: {
     format: 'esm',
     sourcemap: true,
     dir: '.',
     entryFileNames: 'dist/[name].js',
     chunkFileNames: 'dist/_[name].js',
+    manualChunks(id) {
+      if (id.startsWith(path.resolve('src', 'call-thru') + path.sep)) {
+        return 'push-iterator.call-thru';
+      }
+
+      return 'push-iterator';
+    },
     hoistTransitiveImports: false,
     plugins: [
       flatDts({
